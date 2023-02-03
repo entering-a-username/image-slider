294866b288381cac970e497d3eebd2138eda7e3e
entering-a-username <tamunacer@gmail.com>
2023-02-03 16:00:26 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+(function() {
+    const next = document.getElementById("nextID");
+    const previous = document.getElementById("prevID");
+    const images = [
+        "bg1.jpg", "bg2.webp", "bg4.jpg", "bg5.jpg", "bg6.png", "bg7.jpg",
+    ]
+    let counter = 0;
+
+
+    next.addEventListener("click", function() {
+        counter++;
+        if (counter > images.length - 1){counter = 0}
+        document.body.style.backgroundImage = `url('${images[counter]}')`;
+
+    });
+
+    previous.addEventListener("click", function() {
+        counter--;
+        if (counter < 0) {counter = images.length - 1};
+        document.body.style.backgroundImage = `url('${images[counter]}')`;
+    });
+
+
+})();
