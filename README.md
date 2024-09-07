f6ef6f582bb3b002393d1253220e5b620e79d854
entering-a-username <tamunacer@gmail.com>
2024-09-07 22:01:23 +0400

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+a simple project recreating the functionality of an image slider + some tailwindcss
--- script.js
+++ script.js
@@ -3,21 +3,21 @@
     const previous = document.getElementById("prevID");
     const images = [
         "bg1.jpg", "bg2.webp", "bg4.jpg", "bg5.jpg", "bg6.png", "bg7.jpg",
-    ]
-    let counter = 0;
+    ];
 
+    let counter = 0;
 
     next.addEventListener("click", function() {
         counter++;
         if (counter > images.length - 1){counter = 0}
-        document.body.style.backgroundImage = `url('${images[counter]}')`;
+        document.body.style.backgroundImage = `url('img/${images[counter]}')`;
 
     });
 
     previous.addEventListener("click", function() {
         counter--;
         if (counter < 0) {counter = images.length - 1};
-        document.body.style.backgroundImage = `url('${images[counter]}')`;
+        document.body.style.backgroundImage = `url('img/${images[counter]}')`;
     });
 
 
